87e67c4de6afd8f1feb28bcd7f98144a4ee719a9
dhk465 <daehee.kim465@gmail.com>
2025-01-13 21:03:17 +0100

    added parts instead of long list of attributes

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 
 const Header = (props) => {
-  console.log(props);
+  
   return (
     <h1>
       {props.course}
     </h1>
-  )
+  );
+
 };
 
 Header.propTypes = {
@@ -14,13 +15,15 @@ Header.propTypes = {
 };
 
 const Part = (props) => {
+
   return (
     <>
       <p>
         {props.partName} {props.exerciseCount}
       </p>
     </>
-  )
+  );
+  
 };
 
 Part.propTypes = {
@@ -29,42 +32,48 @@ Part.propTypes = {
 };
 
 const Content = (props) => {
+
   return (
     <>
-      <Part partName={props.part1} exerciseCount={props.exercises1} />
-      <Part partName={props.part2} exerciseCount={props.exercises2} />
-      <Part partName={props.part3} exerciseCount={props.exercises3} />
+      {props.parts.map((part, index) => (
+        <Part
+          key={index}
+          partName={part.name}
+          exerciseCount={part.exercises}
+        />
+      ))}
     </>
-  )
+  );
+
 };
 
 Content.propTypes = {
-  part1: PropTypes.string.isRequired,
-  part2: PropTypes.string.isRequired,
-  part3: PropTypes.string.isRequired,
-  exercises1: PropTypes.number.isRequired,
-  exercises2: PropTypes.number.isRequired,
-  exercises3: PropTypes.number.isRequired,
+  parts: PropTypes.array.isRequired,
 };
 
 const Total = (props) => {
+  
+  let sum = 0;
+  props.parts.forEach(part => {
+    sum += part.exercises
+  });
 
   return (
     <>
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises {sum}
       </p>
     </>
-  )
+  );
+
 };
 
 Total.propTypes = {
-  exercises1: PropTypes.number.isRequired,
-  exercises2: PropTypes.number.isRequired,
-  exercises3: PropTypes.number.isRequired,
+  parts: PropTypes.array.isRequired,
 };
 
 const App = () => {
+
   const course = 'Half Stack application development';
   const part1 = {
     name: 'Fundamentals of React',
@@ -78,14 +87,16 @@ const App = () => {
     name: 'State of a component',
     exercises: 14
   }
+  const parts = [part1, part2, part3];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} part2={part2.name} part3={part3.name} exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
+  );
+
 };
 
 export default App;
